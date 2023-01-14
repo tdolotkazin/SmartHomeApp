cc570e6d511ab4e276f3513adcfa48ecda714c45
Timur Dolotkazin <tmdolotkazin@avito.ru>
2023-01-14 17:55:36 +0300

    Initial Commit

---
+++ SmartHomeApp/SmartHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeAppApp.swift
+//  SmartHomeApp
+//
+//  Created by tmdolotkazin on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
